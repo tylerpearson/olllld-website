cc790a999d58b413364853d92102fb67c6671039
Tyler Pearson <ty.pearson@gmail.com>
2014-11-29 00:26:03 -0800

    Add in intro animation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+(function() {
+    var $cover = $('.cover'),
+        $intro = $('.intro');
 
+    $cover.delay(1500).fadeOut(1750, function() {
+        $intro.css('display', 'none');
+    })
+}());
